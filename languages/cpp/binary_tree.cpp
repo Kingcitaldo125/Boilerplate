405c40521af05841ab7adc3418fdac9853296186
Paul <paularelt@gmail.com>
2024-03-31 08:59:06 -0400

    Create binary_tree.cpp
    Add binary tree boilerplate

---
+++ languages/cpp/binary_tree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct TreeNode {
+  TreeNode() : data(-1), left(nullptr), right(nullptr) {}
+  TreeNode(int d) : data(d), left(nullptr), right(nullptr) {}
+
+  int data;
+  TreeNode *left;
+  TreeNode *right;
+};
+
+void inorder(TreeNode *root) {
+  if (root == nullptr) {
+    return;
+  }
+
+  inorder(root->left);
+  std::cout << root->data << "\n";
+  inorder(root->right);
+}
+
+inline void tree_stack() {
+  TreeNode root(4);
+  TreeNode left(2);
+  TreeNode right(7);
+
+  root.left = &left;
+  root.right = &right;
+
+  inorder(&root);
+}
+
+inline void tree_heap() {
+  TreeNode *root = new TreeNode(4);
+  TreeNode *left = new TreeNode(2);
+  TreeNode *right = new TreeNode(7);
+
+  root->left = left;
+  root->right = right;
+
+  inorder(root);
+
+  delete left;
+  delete right;
+  delete root;
+}
+
+int main() { return 0; }
