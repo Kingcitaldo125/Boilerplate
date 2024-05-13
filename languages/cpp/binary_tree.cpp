7b117b14faf15ca25b5d783f2b9d84234e47239d
Paul <paularelt@gmail.com>
2024-05-13 17:48:20 -0400

    Update binary_tree.cpp
    update indent

--- languages/cpp/binary_tree.cpp
+++ languages/cpp/binary_tree.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 
 struct TreeNode {
-  TreeNode() : data(-1), left(nullptr), right(nullptr) {}
-  explicit TreeNode(int d) : data(d), left(nullptr), right(nullptr) {}
+    TreeNode() : data(-1), left(nullptr), right(nullptr) {}
+    explicit TreeNode(int d) : data(d), left(nullptr), right(nullptr) {}
 
-  int data;
-  TreeNode *left;
-  TreeNode *right;
+    int data;
+    TreeNode *left;
+    TreeNode *right;
 };
 
 void inorder(TreeNode *root) {
-  if (root == nullptr) {
-    return;
-  }
+    if (root == nullptr) {
+        return;
+    }
 
-  inorder(root->left);
-  std::cout << root->data << " ";
-  inorder(root->right);
+    inorder(root->left);
+    std::cout << root->data << " ";
+    inorder(root->right);
 }
 
 inline void tree_stack() {
-  TreeNode root(4);
-  TreeNode left(2);
-  TreeNode right(7);
+    TreeNode root(4);
+    TreeNode left(2);
+    TreeNode right(7);
 
-  root.left = &left;
-  root.right = &right;
+    root.left = &left;
+    root.right = &right;
 
-  inorder(&root);
+    inorder(&root);
 }
 
 inline void tree_heap() {
-  TreeNode *root = new TreeNode(4);
-  TreeNode *left = new TreeNode(2);
-  TreeNode *right = new TreeNode(7);
+    TreeNode *root = new TreeNode(4);
+    TreeNode *left = new TreeNode(2);
+    TreeNode *right = new TreeNode(7);
 
-  root->left = left;
-  root->right = right;
+    root->left = left;
+    root->right = right;
 
-  inorder(root);
+    inorder(root);
 
-  delete left;
-  delete right;
-  delete root;
+    delete left;
+    delete right;
+    delete root;
 }
 
 int main() { return 0; }
